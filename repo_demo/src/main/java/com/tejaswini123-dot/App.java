437329d8859e563abd37bc0815ba063547a7a752
unknown <tejaswinir690@gmail.com>
2025-03-13 15:35:40 +0530

    project 8

---
+++ repo_demo/src/main/java/com/tejaswini123-dot/App.java
@@ -0,0 +1,13 @@
+package com.tejaswini123-dot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
